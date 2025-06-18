c80344f139a2158b194bd801d256c6e90a80ee69
luck283 <a394037812@163.com>
2025-06-18 18:11:33 +0800

    更新 fq_vip_optimized.js

--- fq_vip_optimized.js
+++ fq_vip_optimized.js
@@ -1,20 +1,47 @@
-let obj = JSON.parse($response.body || '{}');
+// 番茄小说 VIP 伪装优化脚本
+let body = $response.body;
+let obj;
 
-obj.data = obj.data || {};
+try {
+  obj = JSON.parse(body);
+} catch (e) {
+  $done({});
+  return;
+}
 
-obj.data.vip_status = 1;
-obj.data.is_vip = true;
-obj.data.vip_expire_time = 4102415999;
-obj.data.vip_level = 3;
-obj.data.vip_type = "premium";
-obj.data.rights = {
-  read_without_ads: true,
-  download_allowed: true,
-  exclusive_content: true,
-  watch_vip_video: true
-};
-obj.data.can_read = true;
-obj.data.can_download = true;
-obj.data.has_vip_privileges = true;
+if (obj?.data) {
+  obj.data.vip_type = 1;
+  obj.data.vip_status = 1;
+  obj.data.is_vip = true;
+  obj.data.vip_level = 3;
+  obj.data.expire_time = 4102415999;
+  obj.data.vip_expire_time = 4102415999;
+  obj.data.surplus_days = 999;
+  obj.data.can_read = true;
+  obj.data.can_download = true;
+  obj.data.auto_renewal = true;
 
-$done({body: JSON.stringify(obj)});
+  obj.data.rights = {
+    read_without_ads: true,
+    download_allowed: true,
+    exclusive_content: true,
+    listen_books: true,
+    vip_video: true
+  };
+
+  obj.data.user_subscription = {
+    is_vip: true,
+    level: 3,
+    expire_time: 4102415999,
+    surplus_days: 999,
+    rights: obj.data.rights
+  };
+
+  obj.data.has_vip_privileges = true;
+  obj.data.vip_label = "高级会员";
+  obj.data.vip_name = "超级番茄 VIP";
+
+  $done({ body: JSON.stringify(obj) });
+} else {
+  $done({});
+}
